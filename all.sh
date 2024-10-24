201bd3da5cc3747ba9fd5506a36571187eeb1eff
Javier Lasso <125415549+Jlroja@users.noreply.github.com>
2024-10-24 18:36:21 -0500

    add images and changes

--- all.sh
+++ all.sh
@@ -4,12 +4,14 @@
 # conversion de un PNG a secuencia de pixeles, se procesan esos pixeles y
 # posteriormente se convierte esa secuencia de pixeles a un archivo en formato
 # PNG
-#
-# Autor: John Sanabria - john.sanabria@correounivalle.edu.co
-# Fecha: 2024-08-22
-#
-INPUT_PNG="image.png"
-TEMP_FILE="image.bin"
-python3 fromPNG2Bin.py ${INPUT_PNG}
-./main ${TEMP_FILE}
-python3 fromBin2PNG.py ${TEMP_FILE}.new
+
+
+for INPUT_PNG in img*.jpg; do
+    TEMP_FILE="${INPUT_PNG%.jpg}.bin"  
+    echo "Procesando ${INPUT_PNG}..."
+    
+    python3 fromPNG2Bin.py "${INPUT_PNG}"
+    ./main "${TEMP_FILE}"
+    python3 fromBin2PNG.py "${TEMP_FILE}.new"
+    
+done
